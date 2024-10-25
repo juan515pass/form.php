ed32d2215a92f677dccdf1c5e6a99d4a779f2aad
juan515pass <fc401840@gmail.com>
2024-10-25 14:38:22 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -1,1 +1,10 @@
-
+<?php
+$Identificacio=$_post ['Identificacion'];
+$NomApellido=$_post ['NomApellidos'];
+$Cargo=$_post ['Cargo'];
+$Salario=$_post ['Salario'];
+echo " la identificacion del Empleado es: ".Identificacion;
+echo " Los nombres y apellidos del empleado son: ".NomApellidos;
+echo " El cargo del empleado es: ".Cargo;
+echo " El salario del empleado es: ".Salario;
+?>
