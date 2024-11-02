3bc9e3771c7eca45aedc4d1476983a360fd2d510
juan515pass <fc401840@gmail.com>
2024-11-02 13:50:49 -0500

    Update informacion.php

--- informacion.php
+++ informacion.php
@@ -3,8 +3,8 @@ $Identificacion=$_post ['Identificacion'];
 $NomApellidos=$_post ['NomApellidos'];
 $Cargo=$_post ['Cargo'];
 $Salario=$_post ['Salario'];
-echo " la identificacion del Empleado es: ".Identificacion;
-echo " Los nombres y apellidos del empleado son: ".NomApellidos;
-echo " El cargo del empleado es: ".Cargo;
-echo " El salario del empleado es: ".Salario;
+echo " la identificacion del Empleado es: ".$Identificacion;
+echo " Los nombres y apellidos del empleado son: ".$NomApellidos;
+echo " El cargo del empleado es: ".$Cargo;
+echo " El salario del empleado es: ".$Salario;
 ?>
